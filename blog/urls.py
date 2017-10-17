4a40da0a401352f829fd1b8b41579d090004f17a
peanutink <https://github.com/peanutink>
2017-10-17 12:48:56 +0800

    index and details

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+]
