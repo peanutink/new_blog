95ac4c600d15400200646de0ef1d2a578a58b779
peanutink <https://github.com/peanutink>
2017-10-22 21:07:33 +0800

    comments

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 import markdown
 from .models import Category
+from comments.forms import CommentForm
 
 
 # Create your views here.
@@ -20,7 +21,13 @@ def detail(request, pk):
         'markdown.extensions.codehilite',
         'markdown.extensions.toc',
     ])
-    return render(request, 'blog/detail.html', context={'post': post})
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
 
 
 def archives(request, year, month):
